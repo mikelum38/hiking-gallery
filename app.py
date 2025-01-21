f80847af7a345ec144871cc005482ef73d2e60fd
mikelum38 <michel.lauby38@gmail.com>
2025-01-21 23:54:49 +0100

    pb vercel après deploiement

--- app.py
+++ app.py
@@ -16,7 +16,6 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.FileHandler('app.log', encoding='utf-8'),
         logging.StreamHandler()
     ]
 )
